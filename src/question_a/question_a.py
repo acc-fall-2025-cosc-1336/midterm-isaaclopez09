09a5a6b1279a58141fef7b5e5ce50ea260cc441e
isaaclopez09 <mauro.lopez1@g.austincc.edu>
2025-10-16 06:19:25 +0000

    added return functions

--- src/question_a/question_a.py
+++ src/question_a/question_a.py
@@ -1,3 +1,9 @@
 #write functions here, don't add input('') statements here!
 def test_config():
-    return True
+        return True
+
+def get_assessment_value(property_value):
+        return property_value * .6
+
+def get_tax_assessed(assessment_value):
+        return (assessment_value/100) *.72
