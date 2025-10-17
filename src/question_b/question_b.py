5a4e9035265a05a63dd29a6c2fcc7882f8e732cb
isaaclopez09 <mauro.lopez1@g.austincc.edu>
2025-10-17 02:00:59 +0000

    added return value functions to b

--- src/question_b/question_b.py
+++ src/question_b/question_b.py
@@ -1,1 +1,11 @@
 #write functions here, don't add input('') statements here!
+
+def get_miles_per_hour(kilometers, minutes):
+    
+    
+    miles = kilometers * 0.621371
+    hours = minutes / 60
+    
+    miles_per_hour = miles / hours
+    
+    return miles_per_hour
