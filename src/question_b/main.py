851098ef3ad29bd797801f56e154269c9a9e310f
isaaclopez09 <mauro.lopez1@g.austincc.edu>
2025-10-17 02:01:23 +0000

    added main function to b

--- src/question_b/main.py
+++ src/question_b/main.py
@@ -1,1 +1,20 @@
 #add import
+from question_b import get_miles_per_hour
+
+def main():
+    print("Miles Per Hour Calculator")
+    
+    kilometers = int(input("Enter kilometers: "))
+    if kilometers <= 0:
+        return print("Invalid arguments: Kilometers must be greater than 0")
+
+    minutes = int(input("Enter minutes: "))
+    if minutes <= 0:
+        return print("Invalid arguments: minutes must be greater than 0")
+
+
+    miles_per_hour = get_miles_per_hour(kilometers, minutes)
+
+    print(f"The speed in miles per hour is {miles_per_hour:.6f}")
+
+main()
