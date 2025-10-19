56a441c7e0938e43b6280118732bc23e8d38a12d
isaaclopez09 <mauro.lopez1@g.austincc.edu>
2025-10-19 03:10:59 +0000

    edited main C

--- src/question_c/main.py
+++ src/question_c/main.py
@@ -1,8 +1,10 @@
 #add import
-from question_c import use_global
+from question_c import use_global, global_variable
 
-global_variable = 5
 print(f"Initial value of global_variable: {global_variable}")
 
 use_global()
+
+from question_c import global_variable
+
 print(f"Modified value of global_variable: {global_variable}")
