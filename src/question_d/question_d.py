439ed34600cd79aff178146ffed57c3e760f5ac6
isaaclopez09 <mauro.lopez1@g.austincc.edu>
2025-10-19 01:35:19 +0000

    value return function for D

--- src/question_d/question_d.py
+++ src/question_d/question_d.py
@@ -1,1 +1,15 @@
 #write functions here, don't add input('') statements here!
+
+def is_prime(number):
+    if number <= 1:
+        return False
+    if number == 2:
+        return True
+    if number % 2 == 0:
+        return False 
+    i = 3
+    while i * i <= number:
+        if number % i == 0:
+            return False
+        i += 2
+    return True 
