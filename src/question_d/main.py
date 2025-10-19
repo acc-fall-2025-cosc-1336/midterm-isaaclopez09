e763290e04af501212b299921ffd99f58f6c4883
isaaclopez09 <mauro.lopez1@g.austincc.edu>
2025-10-19 01:35:38 +0000

    main function for D

--- src/question_d/main.py
+++ src/question_d/main.py
@@ -1,1 +1,33 @@
 #add import
+from question_d import is_prime
+
+def main():
+        running = True
+        while running:
+            print("MAIN MENU")
+            print("1 - Prime number calculator")
+            print("2 - Exit")
+            selection = input("Enter menu selection here: ")
+            print("")
+            if selection == "2":
+                 print("Exiting the program")
+                 break
+            elif selection == "1":
+                try:
+                    number = int(input("Enter number to check if it is prime: "))
+                    print("")
+                    if is_prime(number):
+                        print(f"{number} is a prime number.")
+                    else:
+                        print(f"{number} is NOT a prime number.")
+                except ValueError:
+                    print("Please enter a valid integer or 'q' to quit.")
+
+                print("")
+            else:
+                print("Error, enter valid menu selection")
+                print("")
+
+            
+main()
+                  
